605a6b5f5cce326e18aa15f3be798510ce5c08c7
Shakira Pingue <sm880@kent.ac.uk>
2023-12-09 01:20:11 +0000

    Session Six Files

--- session-6/index.js
+++ session-6/index.js
@@ -3,18 +3,18 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+let counter = 0
 
+while ( counter <= 10 ) {
+    const randomNumberWhile = Math.round(Math.random() * 10);
+    console.log(`Random number ${counter}:` + randomNumberWhile);
+    counter = counter +1;   
+};
 
-
-
-
-
-
-
-
-
-
-
+for ( i = 0; i < 11; i++ ) {
+    const randomNumberFor = Math.round(Math.random() * 10);
+    console.log(`Random number ${i}:` + randomNumberFor);
+}
 
 
 // ****************************************************TASK 2****************************************************
