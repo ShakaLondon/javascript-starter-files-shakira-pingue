7be6649cc861dc2dcec02a6e5147205ab3871690
Shakira Pingue <sm880@kent.ac.uk>
2023-12-09 01:10:27 +0000

    Session Two Files

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,8 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+console.log('Hello Everyone!')
+console.log('Hey' + 'how are you?')
+console.log(document.getElementById('description').innerHTML)
+console.log(document.querySelector('#description').innerHTML)
+console.log(document.querySelectorAll('.about')[0].innerHTML)
+console.log(document.querySelectorAll('.about'))
